19f6ade502accbbdf29c673db6db624f00cba3bd
balogun14 <awwalbalogun06@gmail.com>
2023-07-04 07:36:04 +0100

    sync

--- summary/settings.py
+++ summary/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '#b(x0#mi8t(t3kpf)@w6k%h20op3h27t%lbc*70+@h8)j1=+%#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh','127.0.0.1']
 
