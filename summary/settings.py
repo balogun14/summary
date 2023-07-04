4c98b0d217fb2bd5d7307875a8ac501ba91366d1
balogun14 <awwalbalogun06@gmail.com>
2023-07-04 23:54:33 +0100

    i fixed the urls

--- summary/settings.py
+++ summary/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-# SECRET_KEY = '#b(x0#mi8t(t3kpf)@w6k%h20op3h27t%lbc*70+@h8)j1=+%#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
--- summarynotes/urls.py
+++ summarynotes/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path,include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("vrform/",views.vrform , name="vrform"),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
