16d7b9f622c29d46855163ecf887c88c163514c1
balogun14 <awwalbalogun06@gmail.com>
2023-07-10 20:55:29 +0100

    i added django_toolbar

--- summary/settings.py
+++ summary/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "debug_toolbar",
     'summarynotes',
 ]
 
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
--- summary/urls.py
+++ summary/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include('summarynotes.urls'), name="summary")
+    path("", include('summarynotes.urls'), name="summary"),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
