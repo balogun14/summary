789ea239ed36b35322cf97cb26931f32eec05260
balogun14 <awwalbalogun06@gmail.com>
2023-07-10 19:07:32 +0100

    i added the staticfiles

--- summary/settings.py
+++ summary/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh','127.0.0.1']
 
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'summary.urls'
@@ -129,7 +131,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIR = BASE_DIR / "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build/static')
+STATIC_ROOT =  BASE_DIR / "static/"
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
