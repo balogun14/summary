dc945092c7cf2057e043f692c60ef444475a310e
balogun14 <awwalbalogun06@gmail.com>
2023-07-12 06:30:06 +0100

    finally

--- summary/settings.py
+++ summary/settings.py
@@ -44,13 +44,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'summary.urls'
@@ -129,8 +129,12 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static/')
 STATIC_URL = 'static/'
 
-STATICFILES_DIR = BASE_DIR / "static/"
-STATIC_ROOT =  BASE_DIR / "static/"
+STATIC_ROOT =  BASE_DIR / "staticfiles_build/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "summary/static")
+]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
