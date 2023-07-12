884d3a1cc8767d60df5ea1479549ccfa8d824676
balogun14 <awwalbalogun06@gmail.com>
2023-07-12 07:05:48 +0100

    123e

--- summary/urls.py
+++ summary/urls.py
@@ -21,4 +21,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('summarynotes.urls'), name="summary"),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
